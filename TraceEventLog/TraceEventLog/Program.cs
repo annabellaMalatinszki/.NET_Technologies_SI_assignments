28e7cc679b0d03486bb56bf606a79d248e476fa4
annabellaMalatinszki <a.malatinszki@gmail.com>
2017-12-10 16:30:42 +0100

    Instrumentation Hands-on/Exercise 2

---
+++ TraceEventLog/TraceEventLog/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace TraceEventLog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EventLog MyLog = new EventLog("X", "localhost", "Demo");
+            Trace.AutoFlush = true;
+            EventLogTraceListener MyListener = new EventLogTraceListener(MyLog);
+            Trace.WriteLine("This is a test");
+
+            //i dont know how to view the event log :(
+
+        }
+    }
+}
