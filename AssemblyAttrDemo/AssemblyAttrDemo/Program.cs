48c1ccbf349d39fab648bd5f31ca15bb93a7e2d1
annabellaMalatinszki <a.malatinszki@gmail.com>
2017-12-10 13:56:45 +0100

    Reflection Hands-on/Exercise 2

---
+++ AssemblyAttrDemo/AssemblyAttrDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace AssemblyAttrDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            Type assemblyType = typeof(AssemblyDescriptionAttribute);
+            Object[] customAttributes = assembly.GetCustomAttributes(assemblyType, false);
+            if (customAttributes.Length > 0)
+            {
+                AssemblyDescriptionAttribute description = (AssemblyDescriptionAttribute)customAttributes[0];
+                Console.WriteLine(description.Description);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
