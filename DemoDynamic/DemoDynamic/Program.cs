c812accaf47498af5f00a3732a30e8d02d438a08
annabellaMalatinszki <a.malatinszki@gmail.com>
2017-12-10 13:57:56 +0100

    Reflection Hands-on/Exercise 5

---
+++ DemoDynamic/DemoDynamic/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace DemoDynamic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AssemblyName assemblyName = new AssemblyName();
+            assemblyName.Name = "DemoAssembly";
+            assemblyName.Version = new Version("1.0.0.0");
+
+            AppDomain appDomain = AppDomain.CurrentDomain;
+
+            AssemblyBuilder assemblyBuilder = appDomain.DefineDynamicAssembly(assemblyName, AssemblyBuilderAccess.ReflectionOnly);
+
+            ModuleBuilder moduleBuilder = assemblyBuilder.DefineDynamicModule("CodeModule", "DemoAssembly.dll");
+
+            TypeBuilder typeBuilder = moduleBuilder.DefineType("Animal", TypeAttributes.Public);
+
+            Type animal = typeBuilder.CreateType();
+            Console.WriteLine(animal.FullName);
+
+            foreach (MemberInfo info in animal.GetMembers())
+            {
+                Console.WriteLine(" Member ({0}): {1}", info.MemberType, info.Name);
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
